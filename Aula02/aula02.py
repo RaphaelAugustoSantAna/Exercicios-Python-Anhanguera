9e8fbaafb26bf57686c3ac1b37574b03c3e916a0
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2025-09-06 19:12:55 -0300

    estruturas condicionais

---
+++ Aula02/aula02.py
@@ -0,0 +1,17 @@
+#Estruturas condicionais if, else e elif
+idade = 25
+
+ 
+
+if idade < 18:
+
+    print("Menor de idade")
+
+elif idade >= 18 and idade < 65:
+
+    print("Adulto")
+
+else:
+
+    print("Idoso")
+    
