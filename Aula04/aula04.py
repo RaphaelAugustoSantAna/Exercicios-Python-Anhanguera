b06ce5251f407bfe4cab4d79340f77366b0b270b
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2025-09-08 20:14:02 -0300

    iniciando exercícios sobre funções em python

---
+++ Aula04/aula04.py
@@ -0,0 +1,10 @@
+#Função len()
+
+# Calcular o comprimento de uma lista e, em seguida, imprime o resultado com comentários explicativos:
+
+#Cria uma lista de números
+numeros = [1, 2, 3, 4, 5]
+#Usa a função len() para calcular o comprimento da lista 
+comprimento = len(numeros)
+#imprime o comprimento da lista
+print("O comprimento da lista é:", comprimento) 
