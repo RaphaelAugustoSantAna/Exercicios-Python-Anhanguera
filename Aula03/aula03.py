345cdd09cabb83e4886d90d0cb2417c0e064c6a3
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2025-09-08 18:42:20 -0300

    exemplo com break e continue

--- Aula03/aula03.py
+++ Aula03/aula03.py
@@ -1,22 +1,21 @@
-#exemplo com for
+# exemplo com for
 
-numeros = [1,2,3,4,5]
+numeros = [1, 2, 3, 4, 5]
 
 for num in numeros:
     print(num)
 
 
-#exemplo com while
+# exemplo com while
 
 numero = int(input("Digite um número (ou 0 para sair): "))
 
 while numero != 0:
-   if numero % 2 == 0:
-       print("O número é par.")
-   else:
-       print("O número pe ímpar.")
-   numero = int(input("Digite outro numero (ou 0 para sair): "))
-
+    if numero % 2 == 0:
+        print("O número é par.")
+    else:
+        print("O número pe ímpar.")
+    numero = int(input("Digite outro numero (ou 0 para sair): "))
 
 
 # Controle de repetição: range, break e continue
@@ -26,13 +25,26 @@ while numero != 0:
 
 for x in range(5):
     print(x)
-    
+
 # * Limites Inicial e Superior:
 
 for y in range(2, 7):
     print(y)
-    
+
 # * Com Incremento:
 
 for z in range(1, 11, 2):
     print(z)
+
+
+# exemplo com break e continue:
+
+for numero in range(1, 11):
+    if numero % 2 == 0:
+        print(" O primeiro número par encontrado é:", numero)
+        break
+
+for numero in range(1, 11):
+    if numero == 5:
+        continue
+    print(numero)
