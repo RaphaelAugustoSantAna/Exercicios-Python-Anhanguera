9495fde4a14701c6443675029049a6f919eda255
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2025-09-10 19:12:03 -0300

    exemplo com lista

--- Aula05/aula05.py
+++ Aula05/aula05.py
@@ -1,6 +1,14 @@
+# exemplo de operação de sequência
+
 texto = "Explorando a diversidade de linguagens de programação com Python."
 
 print(f"{f"Tamanho do texto = {len(texto)}"}")
 print(f"Python in texto = {'Python' in texto}")
 print(f"Quantidade de e no texto = {texto.count('e')}")
 print(f"As 5 primeiras letras são: {texto[:5]}")
+
+# exemplo de lista
+
+cores = ["vermelho", "azul", "verde", "amarelo", "roxo"]
+for cor in cores:
+    print(f"Posição = {cores.index(cor)}, cor = {cor}")
---
+++ Aula05/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+cores = ["vermelho", "azul", "amarelo", "roxo"]
+for cor in cores:
+    print(f"Posição = {cores.index(cor)}, cor = {cor}")
