938d4acfcdad2f79f8d82402adc1e96f523dea3c
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2025-09-12 19:59:43 -0300

    exercício usando NumPy para manipular arrays

--- Aula06/tempCodeRunnerFile.py
+++ Aula06/tempCodeRunnerFile.py
@@ -1,12 +1,18 @@
-dici_1 = {}
-dici_1["nome"] = "Maria"
-dici_1["idade"] = 25
-# Exemplo 2 - Criação de um dicionário com pares chave: valor
-dici_2 = {"nome": "Maria", "idade": 25}
-# Exemplo 3 - Criação de um dicionário com uma lista de tuplas representando pares chave: valor
-dici_3 = dict([("nome", "Maria"), ("idade", 25)])
-# Exemplo 4 - Criação de um dicionário usando a função built-in zip() e duas listas, uma para as chaves e outra para os valores.
-dici_4 = dict(zip(["nome", "idade"], ["Maria", 25]))
-# Teste se todas as construções resultam em objetos iguais
-print(dici_1 == dici_2 == dici_3 == dici_4)  # Deve imprimir True
-print(dici_1)
+import numpy as np
+
+# Crie um array NumPy de números inteiros
+my_array = np.array([1, 2, 3, 4, 5])
+# Imprima o array
+print("Array original:")
+print(my_array)
+# Realize operações matemáticas com array
+squared_array = my_array**2  # Eleva cada elemento ao quadrado
+sum_of_elements = np.sum(my_array)  # Calcula a soma de todos os elementos
+# Imprima os resultados
+print("\nArray ao quadrado:")
+print(squared_array)
+print("\nSoma dos elementos:")
+print(sum_of_elements)
+# Acesse elementos do array
+element_at_index_2 = my_array[2]  # Acessa o elemento no índice 2
+print("\nElemento no índice 2:", element_at_index_2)
