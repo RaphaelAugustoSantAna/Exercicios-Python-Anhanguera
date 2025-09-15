61abd40fd7c84ecc9fef30288516b8b07d7f0252
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2025-09-15 20:19:19 -0300

    criando um ambiente virtual, instalando o matplotlib

---
+++ Aula08/tempCodeRunnerFile.py
@@ -0,0 +1,19 @@
+# usando biblioteca matplotlib
+
+import matplotlib.pyplot as plt
+
+# Dados
+
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 1, 3, 5]
+
+# Criar um gráfico de linha
+plt.xlabel("Eixo X")
+plt.ylabel("Eixo Y")
+
+# Adicionar um título ao gráfico
+
+plt.title("Exemplo de Gráfico de Linha")
+
+# Mostrar o gráfico
+plt.show()
