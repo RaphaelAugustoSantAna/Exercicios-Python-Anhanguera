278115038a09658035975d67177273578cacf4cd
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2025-09-22 19:17:39 -0300

    criando uma Série a partir de um dicionário

--- aula10.py
+++ aula10.py
@@ -9,3 +9,16 @@ exemplo1 = [10, 20, 30, 40, 50]
 series1 = pd.Series(data=exemplo1)
 
 print(series1)
+
+########################################
+import pandas as pd
+
+# Criando um dicionário com pares chave-valor
+
+Exemplo2 = {"A": 100, "B": 200, "C": 300, "D": 400, "E": 500}
+
+# Criando Séries a partir do dicionário
+
+series2 = pd.Series(data=Exemplo2)
+
+print(series2)
