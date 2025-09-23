ebff36e8a43a4693135555a038596d82acfe4463
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2025-09-23 18:35:26 -0300

    exemplo de visualização com biblioteca Pandas

--- aula12.py
+++ aula12.py
@@ -5,3 +5,14 @@ dados1 = random.sample(range(100), k=20)
 dados2 = random.sample(range(100), k=20)
 
 plt.plot(dados1, dados2)
+
+############################################
+
+import pandas as pd
+
+dados = {"Produto": ["A", "B", "C"], "qtde_vendida": [33, 50, 45]}
+
+df = pd.DataFrame(dados)
+df.plot(x="Produto", y="qtde_vendida", kind="bar")
+# df.plot(x="Produto", y="qtde_vendida", kind="pie")
+# df.plot(x="Produto", y="qtde_vendida", kind="line")
