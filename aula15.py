28b8f735b4f7b5318c788261591e8bdf29e7ee1f
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2025-09-27 06:40:18 -0300

    outro exemplo com assert, agora com cálculo de média

--- aula15.py
+++ aula15.py
@@ -5,3 +5,21 @@ def divide(x, y):
 
 result = divide(6, 0)
 print(result)
+
+# O assert nos mostra o erro e ajuda a detectar o que pode ser uma avaria no código
+
+
+def calcular_media(notas):
+    assert len(notas) > 0, "A lista de notas não pode ser vazia"
+
+    soma = sum(notas)
+    media = soma / len(notas)
+    return media
+
+# Exemplo 1: Lista de notas vazias
+notas_vazias = []
+media = calcular_media(notas_vazias)  # Isso lançará um Assertion Error
+# Exemplo 2: Lista de notas válida
+notas_validas = [8, 7, 9, 6, 8]
+media = calcular_media(notas_validas)  # Isso funcionará corretamente
+print(media)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,7 +1,15 @@
-def divide(x, y):
-    assert y != 0, "Divisão por zero!"
-    return x / y
 
+def calcular_media(notas):
+    assert len(notas) > 0, "A lista de notas não pode ser vazia"
 
-result = divide(6, 0)
-print(result)
+    soma = sum(notas)
+    media = soma / len(notas)
+    return media
+
+# Exemplo 1: Lista de notas vazias
+# notas_vazias = []
+# media = calcular_media(notas_vazias)  # Isso lançará um Assertion Error
+# Exemplo 2: Lista de notas válida
+notas_validas = [8, 7, 9, 6, 8]
+media = calcular_media(notas_validas)  # Isso funcionará corretamente
+print(media)
