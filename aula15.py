bc5f68bada0deca1edbafe55d25e159e6e83763d
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2025-09-27 09:09:28 -0300

    test com unittest

--- aula15.py
+++ aula15.py
@@ -52,3 +52,38 @@ def square(x):
 # Compara a saída obtida com a saída esperada.
 # Se tudo bater, não mostra nada.
 # Se houver erro, mostra onde a saída foi diferente
+
+##########################################################################
+
+import unittest
+
+
+def add(a, b):
+    return a + b
+
+
+class TestAddition(unittest.TestCase):
+    def test_add_positive_numbers(self):
+        self.assertEqual(add(2, 3), 5)
+
+    def test_add_negative_numbers(self):
+        self.assertEqual(add(-2, -3), -5)
+
+
+if __name__ == "__main__":
+    import unittest
+
+    unittest.main(argv=["first-arg-is-ignored"], exit=False)
+    print("Os testes foram executados com sucesso!")
+
+
+# A função add simplesmente soma dois números e cria uma classe que herda de unittest.TestCase.
+# Isso indica que essa classe contém testes unitários.
+# Dentro da classe de teste, você define métodos de teste.
+# Cada método de teste deve começar com a palavra-chave test.
+# Dentro desses métodos, você usa assertivas (como self.assertEqual)
+# para verificar se o comportamento esperado do código é atendido.
+# A condição if __name__ == '__main__':
+# garante que a suíte de testes seja executada somente se o script for executado diretamente
+# (não se for importado como um módulo em outro script).
+# unittest.main() executa todos os testes definidos na classe TestAddition.
