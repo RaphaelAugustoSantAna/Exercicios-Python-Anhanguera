b420db41bdd737084b5ee85fa10df206112cbba1
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2025-09-27 08:45:47 -0300

    teste com doctest

--- aula15.py
+++ aula15.py
@@ -16,6 +16,7 @@ def calcular_media(notas):
     media = soma / len(notas)
     return media
 
+
 # Exemplo 1: Lista de notas vazias
 notas_vazias = []
 media = calcular_media(notas_vazias)  # Isso lançará um Assertion Error
@@ -23,3 +24,31 @@ media = calcular_media(notas_vazias)  # Isso lançará um Assertion Error
 notas_validas = [8, 7, 9, 6, 8]
 media = calcular_media(notas_validas)  # Isso funcionará corretamente
 print(media)
+
+###########################################################3
+
+import doctest
+
+
+def square(x):
+    """
+    Retorna o quadrado de um número
+
+    Exemplos:
+    >>> square(3)
+    9
+    >>> square(-2)
+    4
+    >>> square(0)
+    0
+    """
+    return x * x
+    doctest.testmod()
+
+
+# O doctest permite escrever exemplos de uso dentro da docstring do código (usando >>>).
+# Quando você roda doctest.testmod(), ele:
+# Executa os exemplos da documentação como testes.
+# Compara a saída obtida com a saída esperada.
+# Se tudo bater, não mostra nada.
+# Se houver erro, mostra onde a saída foi diferente
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,15 +1,17 @@
+import doctest
 
-def calcular_media(notas):
-    assert len(notas) > 0, "A lista de notas não pode ser vazia"
 
-    soma = sum(notas)
-    media = soma / len(notas)
-    return media
+def square(x):
+    """
+    Retorna o quadrado de um número
 
-# Exemplo 1: Lista de notas vazias
-# notas_vazias = []
-# media = calcular_media(notas_vazias)  # Isso lançará um Assertion Error
-# Exemplo 2: Lista de notas válida
-notas_validas = [8, 7, 9, 6, 8]
-media = calcular_media(notas_validas)  # Isso funcionará corretamente
-print(media)
+    Exemplos:
+    >>> square(3)
+    9
+    >>> square(-2)
+    4
+    >>> square(0)
+    0
+    """
+    return x * x
+    doctest.testmod()
